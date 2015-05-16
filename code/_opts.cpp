bc9dff8f7f60eb058bde364e08e2b67413c81d7f
Louis Dionne <ldionne.2@gmail.com>
2015-05-16 14:12:03 -0600

    Use _s user-defined literals for strings

--- code/_opts.cpp
+++ code/_opts.cpp
@@ -2,7 +2,13 @@
 // Distributed under the Boost Software License, Version 1.0.
 
 #include <boost/hana.hpp>
+#include <string>
 namespace hana = boost::hana;
+
+
+template <typename CharT, CharT ...s>
+constexpr auto operator"" _s()
+{ return hana::string<s...>; }
 
 
 template <typename ...Options>
@@ -42,35 +48,29 @@ constexpr option_maker<Options..., Option> operator|(option_maker<Options...> op
   return {{hana::append(options.options, opt)}};
 }
 
-auto name = [](auto s) { return hana::make_pair("name", s); };
-auto description = [](auto s) { return hana::make_pair("description", s); };
-auto default_ = [](auto s) { return hana::make_pair("default", s); };
+auto name = [](auto s) { return hana::make_pair("name"_s, s); };
+auto description = [](auto s) { return hana::make_pair("description"_s, s); };
+auto default_ = [](auto s) { return hana::make_pair("default"_s, s); };
 
 
 auto options = option_maker<>{} | banner(
-"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod"
-"tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
-"quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
-"consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse"
-"cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non"
-"proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod"_s
 )
-| option(name("ignore"), description("Ignore incorrect values"))
-| option(name("iters"), description("Number of iterations"), default_(5))
+| option(name("ignore"_s), description("Ignore incorrect values"_s))
+| option(name("iters"_s), description("Number of iterations"_s), default_(5))
 
-| option(name("volume"), description("Volume level"), default_(3.0))
+| option(name("volume"_s), description("Volume level"_s), default_(3.0))
 | check([](auto opts) {
-    if (opts["volume"] < 0)
+    if (opts["volume"_s] < 0)
       opts.die("volume must be non-negative");
   })
 
-| option(name("file"), description("Extra data filename to read in"), hana::type<int>)
+| option(name("file"_s), description("Extra data filename to read in"_s), hana::type<std::string>)
 | check([](auto opts) {
-    if (opts["file"] && !opts["file"].exists())
+    if (opts["file"_s] && !opts["file"_s].exists())
       opts.die("file must exists");
   })
 ;
-
 
 
 int main(int argc, char *argv[]) {
